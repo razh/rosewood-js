1f51c2da1ab9df49516732feb8657f624446b206
Raymond Zhou <somecrazyidiot@gmail.com>
2013-04-23 15:58:18 -0400

    Update regex to better match valid notes. Add an input form to NoteView.

--- app/models/note.js
+++ app/models/note.js
@@ -43,45 +43,51 @@ define(
           note += 12;
         }
 
-        this.set( 'note', note );
-        this.set( 'octave', octave );
+        this.set({
+          note: note,
+          octave: octave
+        });
 
         return this;
       },
 
       toString: function() {
         return Note.names[ this.get( 'note' ) ] +  this.get( 'octave' );
+      },
+
+      fromString: function( string ) {
+        var symbols = Note.regex.exec( string );
+        if ( !symbols ) {
+          return this.set({ note: 0, octave: 0 });
+        }
+
+        var name       = symbols[1] || 0,
+            accidental = symbols[2],
+            octave     = symbols[3] || 0;
+
+        var note = Note.names.indexOf( name );
+        if ( accidental === '#' ) {
+          note++;
+        } else if ( accidental === 'b' ) {
+          note--;
+        }
+
+        octave = Math.min( Math.max( octave, 0 ), 10 ); // Clamp octave.
+
+        return this.set({
+          note: note,
+          octave: octave
+        });
       }
     });
 
     Note.fromString = function( string ) {
-      var symbols = Note.regex.exec( string );
-      if ( !symbols ) {
-        return new Note({ note: 0, octave: 0 });
-      }
-
-      var name       = symbols[1],
-          accidental = symbols[2],
-          octave     = symbols[3];
-
-      var note = Note.names.indexOf( name );
-      if ( accidental === '#' ) {
-        note++;
-      } else if ( accidental === 'b' ) {
-        note--;
-      }
-
-      octave = Math.min( Math.max( octave, 0 ), 10 ); // Clamp octave.
-
-      return new Note({
-        note: note,
-        octave: octave
-      });
+      return new Note().fromString( string );
     };
 
     Note.names = [ "C", "C\u266F", "D", "D\u266F", "E", "F", "F\u266F", "G", "G\u266F", "A", "A\u266F", "B" ];
-    //Splits full note into note name, accidental, and octave (can be a two digit number).
-    Note.regex = /(^[A-G])(\#|b)?(-?\d{0,2}$)/;
+    //Splits full note into note name, accidental, and octave (from 0 to 11).
+    Note.regex = /(^[A-G])(\#|b)?((10|11|[0-9])?$)/;
 
     Note.C       =  0;
     Note.C_SHARP =  1;
--- app/views/note-view.js
+++ app/views/note-view.js
@@ -1,13 +1,15 @@
 define(
   [ 'underscore',
     'backbone',
+    'models/note',
     'text!templates/note-view.html' ],
-  function( _, Backbone, noteTemplate ) {
+  function( _, Backbone, Note, noteTemplate ) {
 
     var NoteView = Backbone.View.extend({
       template: _.template( noteTemplate ),
 
       events: {
+        'keyup .note-input': 'inputNote',
         'click .note-button.add': 'add',
         'click .note-button.subtract': 'subtract'
       },
@@ -15,11 +17,36 @@ define(
       initialize: function() {
         _.bindAll( this, 'render' );
         this.model.bind( 'change', this.render );
+        this.previousValidValue = this.model.toString().replace( '\u266F', '#' );
       },
 
       render: function() {
         this.$el.html( this.template({ note: this.model }) );
         return this;
+      },
+
+      inputNote: function( event ) {
+        var $inputElement = this.$el.find( '.note-input' );
+            value         = $inputElement.val();
+
+        // Only validate input on Enter.
+        if ( event.which === 13 ) {
+          // Replace Unicode value with something the regex can detect.
+          value = value.replace( '\u266F', '#' );
+
+          // Only update if the new value is a valid note.
+          // We do this by testing against the regex and comparing the matched
+          // substring.
+          var matches = value.match( Note.regex );
+          if ( matches && matches[0] === value ) {
+            this.model.fromString( value );
+            this.previousValidValue = value;
+           } else {
+            $inputElement.val( this.previousValidValue );
+          }
+        }
+
+        $inputElement.focus();
       },
 
       add: function() {
