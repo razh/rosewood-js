50b3cf0b6422d40e2198133d5d2544d2fa62aed1
Raymond Zhou <somecrazyidiot@gmail.com>
2013-08-21 19:19:00 -0400

    Update karma configuration file to v0.10.

--- karma.conf.js
+++ karma.conf.js
@@ -1,81 +1,87 @@
 // Karma configuration
+// Generated on Wed Aug 21 2013 17:55:37 GMT-0400 (EDT)
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+  config.set({
 
-
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  REQUIRE,
-  REQUIRE_ADAPTER,
-
-  // included: false -> Load these files with RequireJS.
-  {pattern: 'app/*.js', included: false},
-  // Load all .js files not in components folder.
-  {pattern: 'app/!(components)/*.js', included: false},
-  // Load templates.
-  {pattern: 'app/templates/*.html', included: false},
-    // Load component dependencies.
-  {pattern: 'app/components/backbone/backbone.js', included: false},
-  {pattern: 'app/components/jquery/jquery.js', included: false},
-  {pattern: 'app/components/underscore/underscore.js', included: false},
-  {pattern: 'app/components/requirejs-text/text.js', included: false},
-
-  {pattern: 'test/**/*Spec.js', included: false},
-
-  'test/test-main.js'
-];
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
 
-// list of files to exclude
-exclude = [
-  'app/main.js'
-];
+    // frameworks to use
+    frameworks: ['jasmine', 'requirejs'],
+
+    // Don't use html2js preprocessor for html files.
+    preprocessors: {
+      '**/*.html': []
+    },
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+    // list of files / patterns to load in the browser
+    files: [
+      // included: false -> Load these files with RequireJS instead.
+      {pattern: 'app/*.js', included: false},
+      // Load all .js files not in components folder.
+      {pattern: 'app/!(components)/*.js', included: false},
+      // Load templates.
+      {pattern: 'app/templates/*.html', included: false},
+        // Load component dependencies.
+      {pattern: 'app/components/backbone/backbone.js', included: false},
+      {pattern: 'app/components/jquery/jquery.js', included: false},
+      {pattern: 'app/components/underscore/underscore.js', included: false},
+      {pattern: 'app/components/requirejs-text/text.js', included: false},
+
+      {pattern: 'test/**/*Spec.js', included: false},
+
+      'test/test-main.js'
+    ],
 
 
-// web server port
-port = 9876;
+    // list of files to exclude
+    exclude: [
+      'app/main.js'
+    ],
 
 
-// cli runner port
-runnerPort = 9100;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // web server port
+    port: 9876,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
